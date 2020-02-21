1e234de7a11863e00cc9dc9184fc64a4e1381f7a
qiuqiuqiu3871 <435722748@qq.com>
2020-02-21 12:16:15 +0800

    '使用pxtorem插件'

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    'postcss-pxtorem': {
+      rootValue: 16, //结果为：设计稿元素尺寸/16，比如元素宽320px,最终页面会换算成 20rem
+      propList: ['*'],
+      unitPrecision: 5,
+      selectorBlackList: [/tab-bar/, 'navbar', 'van', ],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0
+    }
+  }
+}
